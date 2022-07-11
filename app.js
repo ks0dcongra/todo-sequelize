977f3c0cf52713c204267542bd293b9abd4bca88
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-11 16:29:01 +0800

    feat: add passport.js detail.hbs index.hbs page

--- app.js
+++ app.js
@@ -8,29 +8,49 @@ const bodyParser = require('body-parser') //可以不用額外載入
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
+
 app.get('/', (req, res) => {
-  res.send('hello world')
-})
-app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 })
 
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
@@ -38,11 +58,33 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
-
 
 app.get('/users/logout', (req, res) => {
   res.send('logout')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
